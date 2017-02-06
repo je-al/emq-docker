4ee96676858bd90c981737db9927b4d86e5b598b
Jorge Arrieta <jorge.arrieta@blanclink.com>
2017-02-06 22:52:13 +0000

    Also use generated base configuration

--- preprocess/init.sh
+++ preprocess/init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 WD=$( cd "$(dirname "$0")" ; pwd -P )
-find /opt/emqttd/etc/plugins -type f -name 'emq_*.conf' | xargs -I {} sh -c "<{} ${WD}/process.py > {}.tpl"
+find /opt/emqttd/etc -type f -name 'emq*.conf' | xargs -I {} sh -c "<{} ${WD}/process.py > {}.tpl"
